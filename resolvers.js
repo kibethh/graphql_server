7098c3a6aaccf89cb5f1720d7bd2c0ed776772ea
kibethh <kibethh@gmail.com>
2022-12-25 09:13:44 +0000

    started on mutation

--- resolvers.js
+++ resolvers.js
@@ -4,6 +4,13 @@ export const resolvers = {
   Query: {
     job: (_root, { id }) => Job.findById(id),
     jobs: () => Job.findAll(),
+    company: (_root, { id }) => Company.findById(id),
+  },
+  Mutation: {
+    createJob: (_root, { input }) => Job.create(input),
+  },
+  Company: {
+    jobs: (company) => Job.findAll((job) => job.companyId === company.id),
   },
   Job: {
     company: (job) => Company.findById(job.companyId),
