33a05bf1175f8e2752b30a1a0025b1c875f322d6
kibethh <kibethh@gmail.com>
2022-12-25 05:17:49 +0000

    init commit

---
+++ resolvers.js
@@ -0,0 +1,11 @@
+import { Company, Job } from "./db.js";
+
+export const resolvers = {
+  Query: {
+    job: (_root, { id }) => Job.findById(id),
+    jobs: () => Job.findAll(),
+  },
+  Job: {
+    company: (job) => Company.findById(job.companyId),
+  },
+};
